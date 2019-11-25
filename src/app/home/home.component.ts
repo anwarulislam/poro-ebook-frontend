ffe14510a2463965e59db2b58f19171ccb7c3ddd
Anwarul Islam <anwaarulislaam@gmail.com>
2019-11-25 22:09:50 +0600

    ...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,37 +11,37 @@ export class HomeComponent implements OnInit {
     {
       name: 'The Poison Killer',
       author: 'Julie Martinez',
-      img: '../../../../assets/img/image 1.png',
+      img: 'assets/img/image 1.png',
       link: ''
     },
     {
       name: 'Very Nice',
       author: 'Marey Dermansky',
-      img: '../../../../assets/img/image 4.png',
+      img: 'assets/img/image 4.png',
       link: ''
     },
     {
       name: 'The Little Mermaid',
       author: 'Hans Christina Anderson',
-      img: '../../../../assets/img/image 2.png',
+      img: 'assets/img/image 2.png',
       link: ''
     },
     {
       name: 'A Love Hate Thing',
       author: 'Hans Christina Anderson',
-      img: '../../../../assets/img/image 3.png',
+      img: 'assets/img/image 3.png',
       link: ''
     },
     {
       name: 'The Arsonist',
       author: 'Chloe Hooper',
-      img: '../../../../assets/img/image 5.png',
+      img: 'assets/img/image 5.png',
       link: ''
     },
     {
       name: 'Very Nice',
       author: 'Marey Dermansky',
-      img: '../../../../assets/img/image 4.png',
+      img: 'assets/img/image 4.png',
       link: ''
     }
   ]
