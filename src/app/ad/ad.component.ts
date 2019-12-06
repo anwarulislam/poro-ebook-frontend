985c7ecbbaebc356856b0c5b00bceba595fc2163
Tech <toufiq.austcse@gmail.com>
2019-12-06 21:55:34 +0600

    changes

--- src/app/ad/ad.component.ts
+++ src/app/ad/ad.component.ts
@@ -11,6 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AdComponent implements OnInit {
 
   dokanForm: FormGroup
+  imageURL: any;
 
   constructor(private data: DataService, private router: Router, private route: ActivatedRoute, private fb: FormBuilder) { }
 
@@ -20,9 +21,38 @@ export class AdComponent implements OnInit {
       description: [null, Validators.required],
       author: [null, Validators.required],
       price: [null, Validators.required],
+      isFree: [false],
       contact: [null, Validators.required],
       thumbnail: [null, Validators.required],
     })
+  }
+
+  onFree() {
+    let isFree = this.dokanForm.get('isFree').value
+    if (isFree) {
+      this.dokanForm.get('price').reset()
+      this.dokanForm.get('price').disable()
+    } else {
+      this.dokanForm.get('price').enable()
+    }
+  }
+
+  // Image Preview
+  showPreview(event) {
+    console.log(event);
+    const file = (event.target as HTMLInputElement).files[0];
+    console.log(file);
+    this.dokanForm.patchValue({
+      // thumbnail: file
+    });
+    this.dokanForm.get('thumbnail').updateValueAndValidity()
+
+    // File Preview
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageURL = reader.result as string;
+    }
+    reader.readAsDataURL(file)
   }
 
   submit() {
